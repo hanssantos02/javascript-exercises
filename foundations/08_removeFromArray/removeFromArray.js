b5aa53dafed2f8c6577069585d2e345d83422f9e
Nexus <hansprielasantos02@gmail.com>
2026-08-21 10:40:20 +0800

    Finish 08_removeFromArray Activity

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+    let arrfiltered = arr;
+    for (const element of elements) {
+        arrfiltered = arrfiltered.filter(item => item !== element);
+    }  
+    return arrfiltered;
 };
 
 // Do not edit below this line
