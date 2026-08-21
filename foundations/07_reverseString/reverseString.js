299c7867d84dc4d1614ffe6a9957f421f194d967
Nexus <hansprielasantos02@gmail.com>
2026-08-21 10:15:38 +0800

    Finish 07_reverseString Activity

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let revstr = '';
+    let position = -1;
+    for (let i = 0; i < str.length; i++) {
+        revstr += str.at(position);
+        position -= 1;
+    }
+    return revstr;
 };
 
 // Do not edit below this line
