8c7d065eeb0262300131065e218014dafb3eaf66
Nexus <hansprielasantos02@gmail.com>
2026-08-21 13:25:50 +0800

    Finish 09_sumAll Activity

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(num, range) {
+    let sum = 0;
+    if (num > range) {
+        [num, range] = [range, num];
+    }
+    if (num < 0 || !Number.isInteger(num) || !Number.isInteger(range)) sum = "ERROR";
+    else {
+        for (let i = num; i <= range; i++) {
+                sum += i;
+        };
+    };
+    
+    return sum;
 };
 
 // Do not edit below this line
