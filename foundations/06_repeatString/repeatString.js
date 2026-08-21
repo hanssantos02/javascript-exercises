544a15cb099c0ea9f796a8ef35216ac0e42f41f9
Nexus <hansprielasantos02@gmail.com>
2026-08-21 09:50:26 +0800

    Finish 06_repeatString Activity

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let str1 = str;
+    if (num === 0) str1 = "";
+    else if (num < 0) str1 = "ERROR";
+    else {
+        for (let i = 1; i < num; i++) {
+        str1 += str;
+        };
+    };  
+    return str1;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
