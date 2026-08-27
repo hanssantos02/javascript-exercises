df138dd25d997969597cfa347cdf2cb540bf94f1
Nexus <hansprielasantos02@gmail.com>
2026-08-27 16:39:49 +0800

    Finish 12_calculator Activity

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const sumArr = arr.reduce((total, item) => {
+    return total + item;
+  }, 0)
+  return sumArr;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const productArr = arr.reduce((total, item) => {
+    return total * item;
+  })
+  return productArr;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	const numbers = [];
+
+  for (let i = 1; i <= n; i++) {
+    numbers.push(i);
+  }
+
+  return numbers.reduce((total, item) => {
+    return total * item;
+  }, 1);
 };
 
 // Do not edit below this line
