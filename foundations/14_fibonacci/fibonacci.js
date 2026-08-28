59db63d39a661d5323a301c63ab9d75c65c00399
Nexus <hansprielasantos02@gmail.com>
2026-08-28 11:39:39 +0800

    Finish 14_fibonacci Activity

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
+    if(num === 0) return 0;
+    else if (num < 0) return "OOPS";
+    else if (num === 1 || num === 2) return 1;
+    const fib = Array(num - 2).fill(0).reduce((arr) => {
+        const prevNum = arr[arr.length - 2];
+        const currentNum = arr[arr.length - 1];
+        const nextNum = prevNum + currentNum;
 
+        arr.push(nextNum);
+
+        return arr;
+    }, [1, 1]);
+
+    return fib[fib.length - 1];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
