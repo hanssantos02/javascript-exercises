8506027ea0f6077814302689a97d246a0976d753
Nexus <hansprielasantos02@gmail.com>
2026-08-28 10:56:18 +0800

    Finish 13_palindromes Activity

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (forward) {
+    forward = forward.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+    const backward = forward
+                            .replace(/[^A-Za-z0-9]/g, "")
+                            .toLowerCase()
+                            .split("")
+                            .reverse()
+                            .join("");
+    return backward === forward;
 };
 
 // Do not edit below this line
