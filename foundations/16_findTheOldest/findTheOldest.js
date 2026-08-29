0c4664c6364a4fd0775b0a44dd714205d35923f3
Nexus <hansprielasantos02@gmail.com>
2026-08-29 16:25:57 +0800

    Finish 16_findTheOldest Activity

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    const oldest = people.reduce((oldestPerson, person) => {
+        const oldestAge = (oldestPerson.yearOfDeath ?? currentYear) - oldestPerson.yearOfBirth;
+        const currentAge = (person.yearOfDeath ?? currentYear) - person.yearOfBirth;
 
+        return currentAge > oldestAge ? person : oldestPerson;
+    });
+
+    return oldest;
 };
 
 // Do not edit below this line
